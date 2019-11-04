854edeeab16275b0810899b7bd84c570ecee29f8
Kleopatra Pirpinia <kleopatra.pirpinia@nedap.com>
2019-11-04 11:08:49 +0100

    add an item to a list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import List from './TodoList'
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: ''
+    }
+  }
+  onChange = (event) => {
+    this.setState({currentItem: event.target.value})
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      currentItem: '',
+      items: [...this.state.items, this.state.currentItem]
+    });
+  }
+  
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>This is our list</h1>
+    <List items={this.state.items}/>
+    <form className="App" onSubmit={this.onSubmit}>
+       <input value={this.state.currentItem} onChange={this.onChange} />
+       <button>Submit</button>
+   </form>
     </div>
-  );
+  );}
+  
 }
 
 export default App;
